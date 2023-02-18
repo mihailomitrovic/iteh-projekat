cb91238ae7dd6b7a379469b1da0cf845fbadea49
saki100 <sara.spanovic10@gmail.com>
2023-02-18 12:46:27 +0100

    Četvrti commit - sign in, edit, delete user

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Home from './core/Home'
-import Menu from './core/Menu'
 import Users from './user/Users'
 import Signup from './user/Signup'
+import Signin from './auth/Signin'
+import EditProfile from './user/EditProfile'
 import Profile from './user/Profile'
+import PrivateRoute from './auth/PrivateRoute'
+import Menu from './core/Menu'
 
 const MainRouter = ({data}) => {
   return (<div>
@@ -13,7 +16,9 @@ const MainRouter = ({data}) => {
         <Route exact path="/" component={Home}/>
         <Route path="/users" component={Users}/>
         <Route path="/signup" component={Signup}/>
-        <Route path="/user/:userId" component={Profile}/>
+        <Route path="/signin" component={Signin}/>
+        <PrivateRoute path="/user/edit/:userId" component={EditProfile}/>
+        <Route path="/user/:userId" component={Profile}/>  
       </Switch>
     </div>)
 }
--- client/core/Menu.js
+++ client/core/Menu.js
@@ -4,6 +4,9 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import HomeIcon from '@material-ui/icons/Home'
+import AddBoxIcon from '@material-ui/icons/AddBox'
+import Button from '@material-ui/core/Button'
+import auth from './../auth/auth-helper'
 import {Link, withRouter} from 'react-router-dom'
 
 const isActive = (history, path) => {
@@ -25,7 +28,35 @@ const Menu = withRouter(({history}) => (
           </IconButton>
         </Link>
       </div>
-      
+      <div style={{'position':'absolute', 'right': '10px'}}><span style={{'float': 'right'}}>
+      {
+        !auth.isAuthenticated() && (<span>
+          <Link to="/signup">
+            <Button style={isActive(history, "/signup")}>Sign up
+            </Button>
+          </Link>
+          <Link to="/signin">
+            <Button style={isActive(history, "/signin")}>Sign In
+            </Button>
+          </Link>
+        </span>)
+      }
+      {
+        auth.isAuthenticated() && (<span>
+          <Link to="/media/new">
+            <Button style={isActive(history, "/media/new")}>
+              <AddBoxIcon style={{marginRight: '8px'}}/> Stream
+            </Button>
+          </Link>
+          <Link to={"/user/" + auth.isAuthenticated().user._id}>
+            <Button style={isActive(history, "/user/" + auth.isAuthenticated().user._id)}>My Profile</Button>
+          </Link>
+          <Button color="inherit" onClick={() => {
+              auth.signout(() => history.push('/'))
+            }}>Sign out</Button>
+        </span>)
+      }
+      </span></div>
     </Toolbar>
   </AppBar>
 ))
